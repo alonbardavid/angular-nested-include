4a9fd7dd50e00109e914f74030ad5478fc7e4d6b
alonb <alonb@kontera.com>
2014-06-22 13:26:34 +0300

    Updates

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,24 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "angularNestedInclude",
+      "shortName": "angularNestedInclude",
+      "type": "object",
+      "moduleName": "angularNestedInclude",
+      "shortDescription": "angularNestedInclude",
+      "keywords": "adding alert alert-warn allows angular-nested-include angularnestedinclude api attribute class decorates depend directives elements finished fire interact loading modify modules nested ng-cinlude ng-include object on-nested-load slowdowns unexpected"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
